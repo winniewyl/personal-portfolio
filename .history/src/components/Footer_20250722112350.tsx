8fd8eede3ac164aec9f86bd0c4eee32b6f774430
Winnie Wang <wangyuling3532@gmail.com>
2025-07-31 11:30:27 +0800

    Remove .history folder to reduce repository size

--- .history/src/components/Footer_20250722112350.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer: React.FC = () => (
-  <footer className="w-full py-4 flex justify-center items-center border-t border-gray-200 dark:border-gray-700 text-gray-500 text-sm mt-8">
-    © {new Date().getFullYear()} Winnie Wang. All rights reserved.
-  </footer>
-);
-
-export default Footer;
